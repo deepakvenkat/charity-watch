f888e5575fd15fc797e09c3d778a957d83333515
deepakvenkat <deepakvenkat@live.com>
2012-08-24 14:28:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharityWatch::Application.config.secret_token = '0e15f1d46f154fbd33f1a10242f28b11742348fdd26e84a7409b308784ab918c49ca08b7c42b8ed361e694a20250d682035d9fd5c91d2de7a6fd899dd50f6ab7'
